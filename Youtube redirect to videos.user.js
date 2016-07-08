d76d97d8d6d20eba9c9bf16c70aa2c87c49a248e
mindm <maindi66@gmail.com>
2016-07-08 18:23:10 +0300

    Add files via upload

---
+++ Youtube redirect to videos.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         Youtube redirect to videos
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://www.youtube.com/*
+// @require      http://code.jquery.com/jquery-latest.js
+// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var pattern = /\/user\/(.*)$/;
+    
+    function redirect(jNode){
+        (jNode.on('click', function(e){
+            e.preventDefault();
+            var linq1 = $(this).attr('href');
+            var linq2 = linq1.replace(pattern, "/$1/videos");
+            location.href = linq2;
+        }));
+    }
+    
+    redirect($('div.yt-lockup-channel div.yt-lockup-content h3 a'));
+    
+    waitForKeyElements ("div.yt-lockup-channel div.yt-lockup-content h3 a", redirect);
+    
+})();
+
+//https://pastee.org/dke83 passu=javascript
